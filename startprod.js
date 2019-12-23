0fad3b48e297483e465bc1f22ef40c57883dc4c0
nikelborm <kolya007.klass@gmail.com>
2019-12-23 20:04:24 +0300

    Новости теперь сораняются в БД
    Фиксил баги, добавил новую фичу

--- startprod.js
+++ startprod.js
@@ -2,10 +2,10 @@ const express = require('express');
 const favicon = require('express-favicon');
 const MongoClient = require("mongodb").MongoClient;
 const path = require('path');
-// const fs = require("fs");
 
 const port = process.env.PORT || 8080;
-const mongoLink = process.env.MONGODB_URI || "mongodb://myUserAdmin:0000@localhost"
+const mongoLink = process.env.MONGODB_URI || "mongodb://myUserAdmin:0000@localhost:27017/admin"
+console.log(process.env.MONGODB_URI);
 
 const mongoClient = new MongoClient(mongoLink, {
     useNewUrlParser: true,
@@ -24,43 +24,28 @@ app.get('/', function (req, res) {
 // const jsonParser = express.json();
 app.post("/load", function (request, response) {
     mongoClient.connect(function (err, client) {
+        if (err) return console.log(err);
         const db = client.db();
         const collection = db.collection("news");
-        if (err) return console.log(err);
 
         collection.find().toArray(function (err, results) {
-            response.send(results);
-            // response.send(fs.readFileSync("newsData.json", "utf8"));
+            response.send(JSON.stringify(results));
         });
         client.close();
     });
 });
 app.post("/send", function (request, response) {
     const data = request.body;
-    const newMsg = {
-        id: data.id,
-        author: data.author,
-        text: data.text,
-        bigText: data.bigText
-    };
     mongoClient.connect(function (err, client) {
         if (err) return console.log(err);
-
         const db = client.db();
         const collection = db.collection("news");
-        collection.insertOne(newMsg, function (err, result) {
-            console.log(err);
-            console.log(result);
-            if (err) {
-                response.send(err);
-            } else {
-                response.send(result);
-            }
+
+        collection.insertOne(data, function (err, result) {
+            if (err) return console.log(err);
+            response.send(JSON.stringify(result));
         });
         client.close();
     });
-    // const parsed = JSON.parse(fs.readFileSync("newsData.json", "utf8"));
-    // const fileContent = JSON.stringify([].concat(newMsg).concat(parsed));
-    // const fileContent = JSON.stringify(parsed.concat(newMsg));
 });
 app.listen(port);
